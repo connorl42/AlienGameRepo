600f31771e052b699033b014d3bf09ff5223b84e
Connor LaForte <143738900+connorl42@users.noreply.github.com>
2026-05-03 12:56:01 -0500

    Added damage system types and interface

---
+++ Plugins/DamageSystem/Source/DamageSystem/DamageSystemTypes.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageSystemTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class EDamageType: uint8
+{
+	None UMETA(DisplayName = "None"),
+	Physical UMETA(DisplayName = "Physical"),
+	Projectile UMETA(DisplayName = "Projectile"),
+	Environmental UMETA(DisplayName = "Enviromental"),
+	StatusCondition UMETA(DisplayName = "StatusCondition"),
+	
+};
+
+UENUM(BlueprintType)
+enum class EDamageResponse : uint8
+{
+	None UMETA(DisplayName = "None"),
+	HitReaction UMETA(DisplayName = "HitReaction"),
+	Knockback UMETA(DisplayName = "Knockback"),
+	Stagger UMETA(DisplayName = "Stagger"),
+	Stun UMETA(DisplayName = "Stun"),
+};
+
+USTRUCT(BlueprintType)
+struct FDamageInfo
+{
+	GENERATED_BODY()
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	float DamageAmount;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	AActor *DamageCauser;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	bool CanBeBlocked = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	bool CanBeParried = true;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	bool ShouldForceInterrupt = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	EDamageType DamageType = EDamageType::None;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	EDamageResponse DamageResponse = EDamageResponse::None;
+};
+
+
